1f332f279d4db634e3b878e16dcfd58123141f80
Lasse Arpe Kristensen <lakri18@student.sdu.dk>
2024-11-26 15:24:05 +0100

    updte

--- hoeffding_vc/js/script.js
+++ hoeffding_vc/js/script.js
@@ -7,3 +7,10 @@ numVal.innerHTML = getVal; // If you don't want the number to be displayed, dele
 slider.oninput = function() {
     numVal.innerHTML = this.value;// If you don't want the number to be displayed, delete this. This is to show at which number the label will change
 }  
+
+
+const mathDiv = document.getElementById('math');
+await MathJax.startup.promise // make sure initial typesetting has taken place
+MathJax.typesetClear([mathDiv]) // clear MathJax awareness of this element
+await MathJax.typesetPromise([mathDiv]) // typeset anew
+
