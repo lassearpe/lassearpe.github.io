167a86de854ba07af40f242005e8212ee9d5e279
Lasse Arpe Kristensen <lakri18@student.sdu.dk>
2024-11-26 12:47:42 +0100

    Tester

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var slider = document.getElementById("range");
+var display = document.getElementById("display");
+var getVal = slider.value;
+
+numVal.innerHTML = getVal; // If you don't want the number to be displayed, delete this. This is to show at which number the label will change
+
