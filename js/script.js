6a8460412801a02bdeccc4604b51ff378fbc57fc
Lasse Arpe Kristensen <lakri18@student.sdu.dk>
2024-11-26 12:55:57 +0100

    Tester

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ var getVal = slider.value;
 
 numVal.innerHTML = getVal; // If you don't want the number to be displayed, delete this. This is to show at which number the label will change
 
+slider.oninput = function() {
+    numVal.innerHTML = this.value;// If you don't want the number to be displayed, delete this. This is to show at which number the label will change
+}  
