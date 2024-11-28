843570a997575a954b47a3fc1ff4c7d0e320bb5f
Lasse Arpe Kristensen <lakri18@student.sdu.dk>
2024-11-28 10:51:32 +0100

    testing mathjax

--- hoeffding_vc/js/script.js
+++ hoeffding_vc/js/script.js
@@ -1,16 +1,31 @@
 var slider = document.getElementById("range");
+var numVal = document.getElementById("numVal");
 var display = document.getElementById("display");
 var getVal = slider.value;
 
-numVal.innerHTML = getVal; // If you don't want the number to be displayed, delete this. This is to show at which number the label will change
+// const rangeInput = document.getElementById('range');
+// const numVal = document.getElementById('numVal');
+
+function updateNumVal() {
+    numVal.textContent = rangeInput.value;
+}
+
+updateNumVal();
+// rangeInput.addEventListener('input', updateNumVal);
+
+
+// numVal.innerHTML = getVal; // If you don't want the number to be displayed, delete this. This is to show at which number the label will change
 
 slider.oninput = function() {
     numVal.innerHTML = this.value;// If you don't want the number to be displayed, delete this. This is to show at which number the label will change
 }  
 
-
 const mathDiv = document.getElementById('math');
-await MathJax.startup.promise // make sure initial typesetting has taken place
-MathJax.typesetClear([mathDiv]) // clear MathJax awareness of this element
-await MathJax.typesetPromise([mathDiv]) // typeset anew
+// await MathJax.startup.promise // make sure initial typesetting has taken place
+// MathJax.typesetClear([mathDiv]) // clear MathJax awareness of this element
+// await MathJax.typesetPromise([mathDiv]) // typeset anew
 
+MathJax.startup.promise.then(() => {
+    MathJax.typesetClear([mathDiv]); // Clear any previous typesetting
+    MathJax.typesetPromise([mathDiv]); // Re-typeset the MathJax content
+});
